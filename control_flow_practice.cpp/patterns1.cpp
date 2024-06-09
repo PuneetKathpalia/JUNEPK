7e30e633c1c0dc18b5628e3ebb256391fdf57f73
Puneet-Kathpalia <puneetkathpalia99@gmail.com>
2024-06-09 20:43:54 +0530

    new file:   control_flow_practice.cpp/patterns1.cpp 	new file:   control_flow_practice.cpp/patterns1.exe

---
+++ control_flow_practice.cpp/patterns1.cpp
@@ -0,0 +1,68 @@
+// *****
+// *****
+// *****
+// *****
+// *****
+
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+    
+// int main(){
+//     int n,m;
+//     cin>>n>>m;
+//     for(int i=1;i<=n;i++)
+//     {
+//         for(int j=1;j<=n;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+        
+//     }
+//     return 0;
+// }
+
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+    
+// int main(){
+//     int n,m;
+//     cin>>n>>m;
+//     for(int i=1;i<=n;i++)
+//     {
+//         for(int j=1;j<=n;j++){
+//             if(i==1 or i==n or j==1 or j==n){
+//             cout<<"*";
+//             }
+        
+//         else{
+//             cout<<" ";
+//         }
+//         }
+//         cout<<endl;
+        
+//     }
+//     return 0;
+//     }
+
+
+// *****
+// ** **
+// * * *
+// ** **
+// *****
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+    
+int main(){
+    
+    return 0;
+}
---
+++ function/1.cpp
@@ -0,0 +1,58 @@
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+//     void cube(int n){
+//         int result=n*n*n;
+//         cout<<result<<" ";
+//     }
+// int main(){
+//     int n;
+//     cin>>n;
+//     cube(n);
+//     return 0;
+// }
+
+// Create a double type array named arr with values: 2.7, 4.3, 12.8, 5.4
+// Print the array values in separate lines
+// Update the 2nd and 4th element to 3.7 and 6.6 respectively
+// Print the array values in separate lines
+
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+    
+// int main(){
+//     double arr[4]={2.7, 4.3, 12.8, 5.4};
+
+// cout<<arr[0]<<endl;
+// cout<<arr[1]<<endl;
+// cout<<arr[2]<<endl;
+// cout<<arr[3]<<endl;
+
+// arr[1]={3.7};
+// arr[3]={6.6};
+// cout<<arr[1]<<endl;
+// cout<<arr[3]<<endl;
+//     return 0;
+// }
+
+// Suppose there are n students. Write a C++ program to input their marks and print their average using a marks array.
+
+// No. of students = 3
+// Marks of student 1 = 89
+// Marks of student 2 = 67
+// Marks of student 3 = 95
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+    
+int main(){
+int arr[3]={89,67,95};
+for(int i=0;i<=3;i++){
+    
+}
+    return 0;
+}
+
+
